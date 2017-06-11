c6837f363e57ab9282643e2587d3dc27816aa0e3
Mor Brisker <morbrisker@gmail.com>
2017-06-11 12:55:49 +0300

    hi bi

--- WebApplication1/jQuery.MazeBoard.js
+++ WebApplication1/jQuery.MazeBoard.js
@@ -1,24 +1,22 @@
 ﻿(function ($) {
     $.fn.drawMaze = function () {
-        this.each(function () {
-            maze = [[0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]];
-            //var myCanvas = document.getElementById("mazeCanvas");
-            var myCanvas = $(this)[0];
-            var context = myCanvas.getContext("2d");
-            var rows = maze.length;
-            var cols = maze[0].length;
-            var cellWidth = myCanvas.width / cols;
-            var cellHeight = myCanvas.height / rows;
-            for (var i = 0; i < rows; i++) {
-                for (var j = 0; j < cols; j++) {
-                    if (maze[i][j] == 1) {
-                        context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
-                    }
+        maze = [[0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]];
+        //var myCanvas = document.getElementById("mazeCanvas");
+        var myCanvas = $(this);
+        var context = myCanvas.getContext("2d");
+        var rows = maze.length;
+        var cols = maze[0].length;
+        var cellWidth = myCanvas.width / cols;
+        var cellHeight = myCanvas.height / rows;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (maze[i][j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
                 }
             }
-        });
+        }
         return this;
     };
-})(jQuery);
+}) (jQuery);
